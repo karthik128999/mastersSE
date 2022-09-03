4a42c2f6b4168da93e24501e87e4132caf9ffdb1
Rohith Dharuru <rdharuru@albany.edu>
2022-09-03 17:21:42 -0400

    Implement Addition method

--- Calc.java
+++ Calc.java
@@ -27,7 +27,7 @@ public class Calculator
     } 
     public int addition(int x, int y)
     {
-       //return x + y;
+       return x + y;
     }
     public int multiplication(int x, int y) 
     {     
